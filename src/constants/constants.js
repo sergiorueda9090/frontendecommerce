18fe9c51135596aa3420512300a345aee81a9472
sergio rueda <sergiorueda90@hotmail.com>
2024-07-31 16:42:19 -0500

    URL PRODUCTION

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,7 @@
 const global = {
-    "token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Im1hdXJpY2lvQGhvdG1haWwuY29tIiwiaWF0IjoxNzIyMjg4MzcwLCJleHAiOjE3MjIzNzQ3NzB9.OmXXRoIjPp62hLhJRqL5LDGW49rOGSbbmynKYfUdCB0",
-    "url": "http://ecommerce/public/"
+    "token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Im1hdXJpY2lvQGhvdG1haWwuY29tIiwiaWF0IjoxNzIyNDYyMDc4LCJleHAiOjE3MjI1NDg0Nzh9.AadI1APrcP68L7RAvB0VVQFOTXYfKtSZAXvqF-zpYac",
+    "url": "http://34.195.84.240/ecommerce/public/",
+    "urlproduction": "http://34.195.84.240/ecommerce/public/"
 }
 
 export default global;
